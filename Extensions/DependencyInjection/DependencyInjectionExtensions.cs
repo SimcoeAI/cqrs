b74ac481023b690526f4844fbfd233bc354f9fd5
Arash A. Sabet <Arash-Sabet@users.noreply.github.com>
2020-11-01 12:08:47 -0500

    Fixed the structure of the repo #3 and added DI extensions #4

---
+++ Extensions/DependencyInjection/DependencyInjectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace SimcoeAI.Abstractions.CQRS.Extensions.DependencyInjection
+{
+    public static class DependencyInjectionExtensions
+    {
+		public static IServiceCollection AddScopedLifetimeCQRS<T>(this IServiceCollection services)
+			=> services.Scan(scan => scan
+				.FromAssemblyOf<T>()
+				.AddClasses(classes => classes.AssignableTo<IBaseCQRS>())
+				.AsImplementedInterfaces()
+				.WithScopedLifetime());
+
+		public static IServiceCollection AddSingletonLifetimeCQRS<T>(this IServiceCollection services)
+			=> services.Scan(scan => scan
+				.FromAssemblyOf<T>()
+				.AddClasses(classes => classes.AssignableTo<IBaseCQRS>())
+				.AsImplementedInterfaces()
+				.WithSingletonLifetime());
+
+		public static IServiceCollection AddTransientLifetimeCQRS<T>(this IServiceCollection services)
+			=> services.Scan(scan => scan
+				.FromAssemblyOf<T>()
+				.AddClasses(classes => classes.AssignableTo<IBaseCQRS>())
+				.AsImplementedInterfaces()
+				.WithTransientLifetime());
+	}
+}
